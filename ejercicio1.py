4d9ef4018c07648caca95f9302f48cd8e7331249
unknown <adrianamorenocard@gmail.com>
2022-09-18 20:43:22 -0300

    punto uno primer ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        módulo1
+# Purpose:
+#
+# Author:      Jorgito
+#
+# Created:     18/09/2022
+# Copyright:   (c) Jorgito 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    x=10
+    y=56
+    suma=(x+y)
+#Imprimir la suma de x mas y
+    print(suma)
+
+if __name__ == '__main__':
+    main()
