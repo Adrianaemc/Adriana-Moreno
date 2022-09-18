f71c7ad7a967b1d834acd58e2f6ebe61067d855b
unknown <adrianamorenocard@gmail.com>
2022-09-18 20:59:11 -0300

    segurndo punto ejercicio 1

---
+++ ejercicio2.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        módulo1
+# Purpose:
+#
+# Author:      Jorgito
+#
+# Created:     18/09/2022
+# Copyright:   (c) Jorgito 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    Nombre= "Adriana"
+    Apellido= "Moreno"
+    Nombrecompleto = Nombre + ", " + Apellido
+#le agrego la coma para que al imprimir separe el nombre del apellido
+    print(Nombrecompleto)
+if __name__ == '__main__':
+    main()
