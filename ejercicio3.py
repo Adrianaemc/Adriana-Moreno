56224e9ac15b1f33f29ed324a7687df241ccdd2e
unknown <adrianamorenocard@gmail.com>
2022-09-18 21:11:04 -0300

    tercer punto primer ejercicio

---
+++ ejercicio3.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        módulo1
+# Purpose:
+#
+# Author:      Jorgito
+#
+# Created:     18/09/2022
+# Copyright:   (c) Jorgito 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    nombre= input("Ingrese su nombre")
+#el usuario debe ingresae su nombre
+    DNI= input("Ingrese su numero de DNI")
+#el usuario debe ingresae su dni
+    print(f'El nombre del usuario es {nombre}')
+    print(f'su DNI es {DNI}')
+if __name__ == '__main__':
+    main()
